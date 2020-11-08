ce7b83257e7739c68bd10be8f0f27303ebfcf709
Anki233 <57432569+Anki233@users.noreply.github.com>
2020-11-08 16:26:29 +0800

    Add files via upload

---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="todo-list">
+        <todo-listItem 
+            v-for="todo in todos" 
+            :key="todo.id" 
+            :todo-item="todo"
+            @change-state="todo.completed = $event.target.checked">
+        </todo-listItem>
+    </div>
+</template>
+
+<script>
+import TodoListItem from '@/components/TodoListItem.vue';
+
+export default {
+    name: "TodoList",
+    components: { TodoListItem },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+</style>
